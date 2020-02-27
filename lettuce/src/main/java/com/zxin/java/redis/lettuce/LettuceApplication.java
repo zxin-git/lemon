16aa0db3122e0f3e334f6ea30e73fbd97555525b
zxin <zxin.info@gmail.com>
2020-02-27 09:43:42 +0800

    Initial commit

---
+++ lettuce/src/main/java/com/zxin/java/redis/lettuce/LettuceApplication.java
@@ -0,0 +1,13 @@
+package com.zxin.java.redis.lettuce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LettuceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LettuceApplication.class, args);
+    }
+
+}
